0596ff43a3224cecda25ad22cd02f491ecee53f0
FTFati <fatima.amkachou@gmail.com>
2024-05-15 08:01:24 -0700

    modification in Flasck Application

--- app_factory.py
+++ app_factory.py
@@ -1,23 +1,24 @@
 from flask import Flask
-from flask_mail import Mail
 from flask_sqlalchemy import SQLAlchemy
 
 # Create the Flask app instance
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sqlite.db'
-app.config['SECRET_KEY'] = 'dfe7b0946804edf295050cbb8ce8d3aec72063aede88df37'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# Initialize SQLAlchemy
+# Configure the SQLite database URI
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sqlite.db'
+
+# Set the secret key for session security
+app.config['SECRET_KEY'] = 'dfe7b0946804edf295050cbb8ce8d3aec72063aede88df37'
+
+# Initialize SQLAlchemy to work with the Flask app
 db = SQLAlchemy(app)
 
-# # Import views/routes
-# from app import *
+# Defineroutes and views below
+@app.route('/')
+def home():
+    return 'Welcome to One Click Project'
 
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'belloabiodun17@gmail.com'
-app.config['MAIL_PASSWORD'] = 'ekjm xvvi ofwn dwxh'
+# Add more routes and views as needed
 
-mail = Mail(app)
+if __name__ == "__main__":
+    app.run(debug=True)
